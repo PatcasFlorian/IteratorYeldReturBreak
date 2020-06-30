3d71ddbe037975ca214bd4a33d899e6adb817751
Patcas Florian <patcasf12@gmail.com>
2020-06-30 18:55:40 +0300

    TemaYieldReturn

---
+++ Iterator/Iterator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    class Program
+    {
+        public static IEnumerable<int> GetMyMaxValue()
+        {
+
+            int maxValue = Int32.MaxValue;
+
+            for (int items = 0; items <= maxValue; items++)
+            {
+                if (items == maxValue / 50000)
+                {
+                    Console.WriteLine("Using yield break");
+                    yield break;
+                }
+
+                yield return items;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+
+            foreach (var i in GetMyMaxValue())
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
